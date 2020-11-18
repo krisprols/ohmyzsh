e1b4feaae680abf12b1cc5df36306a9016bddc75
Christophe Lecoq <39101348+krisprols@users.noreply.github.com>
2020-11-18 15:10:36 +0100

    rclone autocompletion support

--- plugins/rclone/rclone.plugin.zsh
+++ plugins/rclone/rclone.plugin.zsh
@@ -3,5 +3,6 @@
 # Docs: https://rclone.org/commands/rclone_genautocomplete_zsh/
 
 if [ $commands[rclone] ]; then
-  source <(rclone genautocomplete zsh /dev/stdout)
+  source <(rclone genautocomplete zsh /tmp/rclone-plugin.$$ && cat /tmp/rclone-plugin.$$)
+  rm /tmp/rclone-plugin.$$
 fi
